2d76d2099d3d5e37a6045764a3d7e91b8682f41b
SwordOfNayru <plouisberquez@gmail.com>
2022-07-02 13:48:39 +0200

    Init projet

---
+++ src/main/java/com/casi_projet/App.java
@@ -0,0 +1,13 @@
+package com.casi_projet;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
